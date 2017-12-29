8176d6624c9bff61dfd2e79e0c0f52b397bcb45f
Jackson Seven Delta <jacksonsevendelta@gmail.com>
2017-12-29 15:00:48 +1100

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,13 @@
+# Setup and Installation script from https://elinux.org/RPi-Cam-Web-Interface#Basic_Installation
+# Code under a Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) license.
+
+# Run the following commands if you do not have Git
+# -------------------------------------------------
+#sudo apt-get update
+#sudo apt-get dist-upgrade
+#sudo apt-get install git
+# -------------------------------------------------
+
+git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
+cd RPi_Cam_Web_Interface
+./install.sh
